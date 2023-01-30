5b9530c0b73d553e82bcbf20e2c2f6d3975709af
AndrewR3K <andrew.martin.louis@gmail.com>
2023-01-30 13:29:23 -0800

    refactor + optimizations

--- server.lua
+++ server.lua
@@ -5,34 +5,51 @@ TriggerEvent("getCore", function(core)
 end)
 
 
-RegisterServerEvent("onduty")
-AddEventHandler("onduty", function()
-    local _source = source
-    local player = VORPcore.getUser(_source).getUsedCharacter
-    local job = player.job
+-- @param _src = player server source
+-- @param state = true means on duty, false mean off duty
+local function ChangeDuty(_src, state)
+    local player = VORPcore.getUser(_src).getUsedCharacter
+    local currentjob = player.job
+
+    local jobcheck
+    local jobswap
+    local messages = {
+        success = '',
+        failure = ''
+    }
+
     for k, v in pairs(Config.Jobs) do
-        if job == v.offdutyjob then
-            player.setJob(v.job)
-            VORPcore.NotifyBottomRight(_source,"You went on duty",4000)
-        elseif job == v.job then
-            VORPcore.NotifyBottomRight(_source,"You are already on duty",4000)
+        if state then
+            jobcheck = v.offdutyjob
+            jobswap = v.job
+            messages.success = 'You went on duty'
+            messages.failure = 'You are already on duty'
+        else
+            jobcheck = v.job
+            jobswap = v.offdutyjob
+            messages.success = 'You went off duty'
+            messages.failure = 'You are already off duty'
+        end
+
+        if currentjob == jobcheck then
+            player.setJob(jobswap)
+            VORPcore.NotifyBottomRight(_src, messages.success, 4000)
+
+            break --break out of the loop as you found what you wanted, no need to keep looping as this wastes resources.
+        elseif currentjob == v.job then
+            VORPcore.NotifyBottomRight(_src, messages.failure, 4000)
+
+            break --break out of the loop as you found what you wanted, no need to keep looping as this wastes resources.
         end
     end
+end
+
+RegisterCommand("goingonduty", function(source, args, rawCommand)
+    local _source = source
+    ChangeDuty(_source, true)
 end)
 
-
-
-RegisterServerEvent("offduty")
-AddEventHandler("offduty", function()
+RegisterCommand("goingoffduty", function(source, args, rawCommand)
     local _source = source
-    local player = VORPcore.getUser(_source).getUsedCharacter
-    local job = player.job
-    for k, v in pairs(Config.Jobs) do
-        if job == v.job then
-            player.setJob(v.offdutyjob)
-            VORPcore.NotifyBottomRight(_source,"You went off duty",4000)
-        elseif job ==v.offdutyjob then
-            VORPcore.NotifyBottomRight(_source,"You are already off duty",4000)
-        end
-    end
+    ChangeDuty(_source, true)
 end)
